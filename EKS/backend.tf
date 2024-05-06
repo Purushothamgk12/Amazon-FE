d94b0cc193c453feb590d83a3a54452c035d22fe
Purushothamgk12 <109778664+Purushothamgk12@users.noreply.github.com>
2024-05-06 21:55:14 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ravi-project-s3bucket"  # Replace with your actual S3 bucket name
+    bucket = "eks-tf-s3-bucket"  # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"  
-    region = "ap-south-1" 
+    region = "us-east-1" 
   }
 }
